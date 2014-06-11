f574c88f0383b9cd86de273b9778e3c5b50d6d5b
Daniel Greenfeld <pydanny@gmail.com>
2014-06-11 08:32:52 -0700

    Add wheel support with publish, and tested Python versions in trove classifiers

--- setup.py
+++ setup.py
@@ -9,10 +9,22 @@ README = open(os.path.join(here, 'README.rst')).read()
 VERSION = '0.5.1'
 
 
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist bdist_wheel upload')
+    print "You should also consider tagging a release:"
+    print "  git tag -a %s -m 'version %s'" % (VERSION, VERSION)
+    print "  git push --tags"
+    sys.exit()
+
+
 setup(
     name='nose-picker',
     version=VERSION,
-    classifiers=['License :: OSI Approved :: BSD License'],
+    classifiers=[
+        'License :: OSI Approved :: BSD License'
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
     long_description=README,
     url='https://github.com/eventbrite/nose-picker',
     license='BSD',
